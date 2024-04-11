13eacf44acd27da75935bde892589aa2ee65b792
Guillaumeedpt <166716442+Guillaumeedpt@users.noreply.github.com>
2024-04-11 14:40:05 +0200

    Update wordcloud_g2.py

--- wordcloud_g2.py
+++ wordcloud_g2.py
@@ -14,7 +14,48 @@ Original file is located at
 # exercice 2
 # Guillaume
 # state: ongoing
+!pip install pillow
+import numpy as np 
+from PIL import Image,ImageOps
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud,STOPWORDS,ImageColorGenerator
+from scipy.ndimage import gaussian_gradient_magnitude 
 
+# Download the Shakespeare text from the desktop
+file=open('romeo.txt')
+text=file.read()
+
+# Create the WordCloud
+canvas_width=1920
+canvas_height=1080 
+
+# Generate wordcloud
+wordcloud = WordCloud(width=canvas_width,height=canvas_height).generate(text)
+wordcloud.to_file("simple_wordcloud.png") 
+plt.figure(figsize = (10, 10), facecolor=None)
+
+# Save the output wordcloud in png format
+plt.imshow(wordcloud, interpolation='bilinear')
+
+# Show the image output 
+plt.axis("off") 
+plt.tight_layout(pad = 0)
+plt.show() 
+
+#--------additional tricks----
+# By default wordcloud generates random patterns but you can fix the seed
+# Generate wordcloud
+# Replace 1 with any number to get different result
+canvas_width=1920
+canvas_height=1080 
+wordcloud = WordCloud(width=canvas_width,height=canvas_height).generate(text)
+wordcloud = WordCloud(random_state=1).generate(text) 
+wordcloud.to_file("simple_wordcloud.png") 
+plt.figure(figsize = (10, 10), facecolor=None)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off") 
+plt.tight_layout(pad = 0)
+plt.show() 
 # exercice 3
 # Elodie
 # state: ongoing
